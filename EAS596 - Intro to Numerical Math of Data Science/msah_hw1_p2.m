6c338af2a2806294bcc0575b0153ae22394bb264
Sah <matthewsah@hotmail.com>
2019-09-03 19:47:57 -0400

    #Homework2 #Ready

--- EAS596 - Intro to Numerical Math of Data Science/msah_hw1_p2.m
+++ EAS596 - Intro to Numerical Math of Data Science/msah_hw1_p2.m
@@ -1,2 +1,17 @@
+ 
+format long
 clc
 clear
+
+x = pi/ 3 
+h = logspace(-16 ,  -1 )
+n = length(h)
+
+
+for q = 1 : n  
+    functionF(q) = ( sin(x + h(q)) - sin(x)) / h(q) ;
+    O(q) = abs(cos(x) -  functionF(q));
+    
+end
+
+loglog(h , O)
