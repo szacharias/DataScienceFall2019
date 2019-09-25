5d9d3180a3abd7ef90d6f708f973822c5e739008
Matthew <matthewsah@hotmail.com>
2019-09-25 14:08:42 -0400

    #0925
    Edits for friends on Hw1 and Starting HW2

---
+++ EAS596 - Intro to Numerical Math of Data Science/HW3/msah_hw3_p2.m
@@ -0,0 +1,16 @@
+function [length] = msah_hw3_p2(A, i, j)
+ 
+clc
+format long
+
+
+
+
+% Variables 
+% A is adjancy matrix 
+% i is departure point
+% j mininum path length
+
+
+% Error
+% if path > 20 , return error
