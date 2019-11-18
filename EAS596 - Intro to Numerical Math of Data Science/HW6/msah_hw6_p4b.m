eced3316639fe8676e3ba42afa74bc9035235351
Sah <matthewsah@hotmail.com>
2019-11-18 02:21:34 -0500

    #1118 HW6 Q4 complete

--- EAS596 - Intro to Numerical Math of Data Science/HW6/msah_hw6_p4b.m
+++ EAS596 - Intro to Numerical Math of Data Science/HW6/msah_hw6_p4b.m
@@ -1,7 +1,27 @@
-function [outputArg1,outputArg2] = msah_hw6_p4b(inputArg1,inputArg2)
-%MSAH_HW6_P4B Summary of this function goes here
-%   Detailed explanation goes here
-outputArg1 = inputArg1;
-outputArg2 = inputArg2;
+function msah_hw6_p4b(f,p)
+
+
+file_name = f ;
+p_values = p ;
+
+round_times  = size(p_values, 2 )  ;
+row_count_prelim = round_times + 1 ;
+
+rows = ceil( row_count_prelim / 3 ) ;
+
+subplot(rows, 3, 1)
+imshow(file_name)
+title("Original")
+
+
+for i = 1 : round_times
+    current_value = p_values(i);
+    img = msah_hw6_p4a(file_name , current_value);
+    subplot(rows, 3, i+1)
+    imshow(img)
+    title(sprintf('Rank = %d', current_value))
+
+end
+
 end
 
--- EAS596 - Intro to Numerical Math of Data Science/HW6/msah_hw6_p4c.m
+++ EAS596 - Intro to Numerical Math of Data Science/HW6/msah_hw6_p4c.m
@@ -1,7 +1,38 @@
-function [outputArg1,outputArg2] = msah_hw6_p4c(inputArg1,inputArg2)
-%MSAH_HW6_P4C Summary of this function goes here
-%   Detailed explanation goes here
-outputArg1 = inputArg1;
-outputArg2 = inputArg2;
+
+
+pic1 = "UB.png";
+pic2 = "futurama.png";
+pic3 = "square.png";
+
+pic_list = [pic1, pic2 , pic3]
+
+round_size = size(pic_list, 2)
+
+for i = 1 : round_size
+
+    img_filename = imread(pic_list(i));   
+
+    [m , n , c ] = size(img_filename);
+    
+    base_number = min(m , n );
+ 
+     
 end
 
+p_value_pic1 = [1, 5 , 10 , 50 , 100 , 200, 300 ];
+p_value_pic2 = [1 ,5 ,  10 , 50,  100, 200 ,  300, 500 , 1000 ] ;
+p_value_pic3 = [1 , 2 , 5 ,7 , 10  ] ;
+p_value_list = [p_value_pic1 , p_value_pic2 , p_value_pic3];
+
+figure('Name',pic1,'NumberTitle','off');
+msah_hw6_p4b(pic1, p_value_pic1) 
+% % 50
+     
+figure('Name',pic2,'NumberTitle','off');
+msah_hw6_p4b(pic2, p_value_pic2) 
+% % 100
+
+
+figure('Name',pic3,'NumberTitle','off');
+msah_hw6_p4b(pic3, p_value_pic3) 
+% % > 1 
